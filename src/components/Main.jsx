5b3f395cdb28758845b23a2775fc15f20775819b
Jack Day <jack94day@gmail.com>
2025-02-10 15:48:08 +0000

    Add conditional rendering to render ingredients section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,32 +1,35 @@
-import React from "react"
+import React from "react";
 
 export default function Main() {
+  const [ingredients, setIngredients] = React.useState([]);
 
-    const [ingredients, setIngredients] = React.useState([])
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
 
-    const ingredientsListItems = ingredients.map(ingredient => (
-        <li key={ingredient}>{ingredient}</li>
-    ))
+  function addIngredient(formData) {
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  }
 
-    function handleSubmit(formData) {
-        const newIngredient = formData.get("ingredient")
-        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
-    }
+  return (
+    <main>
+      <form action={addIngredient} className="add-ingredient-form">
+        <input
+          type="text"
+          placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+          name="ingredient"
+        />
+        <button>Add ingredient</button>
+      </form>
 
-    return (
-        <main>
-            <form action={handleSubmit} className="add-ingredient-form">
-                <input 
-                    type="text" 
-                    placeholder="e.g. oregano" 
-                    aria-label="Add ingredient"
-                    name="ingredient"
-                />
-                <button>Add ingredient</button>
-            </form>
-            <ul>
-                {ingredientsListItems}
-            </ul>
-        </main>
-    )
+      {ingredients.length > 0 ? (
+        <section>
+          <h2>Your Ingredients:</h2>
+          <ul aria-live="polite">{ingredientsListItems}</ul>
+        </section>
+      ) : null}
+    </main>
+  );
 }
