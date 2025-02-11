7f9798acc270d771fd390e15b86591fa3ffea45f
Jack Day <jack94day@gmail.com>
2025-02-11 18:27:41 +0000

    Add error check for empty and duplicate list items and reformat list display

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,14 +2,22 @@ import React from "react";
 
 export default function Main() {
   const [ingredients, setIngredients] = React.useState([]);
+  const [errorMessage, setErrorMessage] = React.useState("");
 
   const ingredientsListItems = ingredients.map((ingredient) => (
     <li key={ingredient}>{ingredient}</li>
   ));
 
   function addIngredient(formData) {
-    const newIngredient = formData.get("ingredient");
-    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+    const newIngredient = formData.get("ingredient").trim();
+    if (ingredients.includes(newIngredient)) {
+      setErrorMessage(`You've already added ${newIngredient}.`);
+    } else if (newIngredient === "") {
+      setErrorMessage("Please enter an ingredient.");
+    } else {
+      setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+      setErrorMessage("");
+    }
   }
 
   return (
@@ -24,10 +32,14 @@ export default function Main() {
         <button>Add ingredient</button>
       </form>
 
+      {errorMessage && <p style={{ color: "red", fontWeight: "500", textAlign: "center" }}>{errorMessage}</p>}
+
       {ingredients.length > 0 ? (
         <section>
           <h2>Your Ingredients:</h2>
-          <ul aria-live="polite">{ingredientsListItems}</ul>
+          <ul aria-live="polite" className="ingredients-list">
+            {ingredientsListItems}
+          </ul>
           {ingredients.length >= 4 ? (
             <div className="show-recipe-container">
               <div>
@@ -36,7 +48,7 @@ export default function Main() {
               </div>
               <button>Get a recipe</button>
             </div>
-            ) : null}
+          ) : null}
         </section>
       ) : null}
     </main>
